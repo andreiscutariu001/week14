ec31cb52b75b0e999823900043f8acae1f2815db
Andrei Scutariu <andrei.scutariu@endava.com>
2019-03-15 18:32:16 +0200

    add simple thread example

---
+++ WantsomeThreads/WantsomeThreads.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace WantsomeThreads.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var thread = new Thread(() =>
+            {
+                Console.WriteLine($"Hello from created thread with id - {Thread.CurrentThread.ManagedThreadId}");
+            });
+            thread.Start();
+
+            Console.WriteLine($"Hello from main thread with id - {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.ReadKey();
+        }
+    }
+}
